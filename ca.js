0e595a8c20391651825ca0c1e38ec7e1906d963c
Tim McCormack <cortex@brainonfire.net>
2022-12-29 16:58:37 -0500

    Reorg and comment; add config for changing drawing element; add edge type
    Also, make rabbit opaque for when it is drawn over sand.

--- ca.js
+++ ca.js
@@ -1,7 +1,15 @@
+// Definitions:
+//
+// - pos: A world position [x, y] -- cell coordinates, rather than pixel coordinates
+// - dat: Cell data, a map that contains at least a 'type' field ("air", "sand", etc.)
+// - cell: A position and its contents, as {pos, dat}
+
 "use strict";
+
 
 // ==== General utilities ==== //
 
+// Return a shuffled copy of an array.
 function shuffled(array) {
     // Slower than Fisher-Yates, but good enough for precomputation.
     return array
@@ -10,17 +18,21 @@ function shuffled(array) {
         .map(({ value }) => value);
 }
 
+// Make an array [0, len).
+function range(len) {
+    // stupid JS hacks
+    return Array.from(new Array(len), (_, i) => i);
+}
 
-// Definitions:
-//
-// - pos: A world position [x, y]
-// - dat: Cell data, a map that contains at least a type field ("air", "sand", etc.)
-// - cell: A position and its contents, as {pos, dat}
+// ==== DOM references ==== //
 
 const playPause = document.getElementById("play-pause");
 
 const canvas = document.getElementById("world");
 const ctx = canvas.getContext('2d'); // drawing context
+
+
+// ==== World settings ==== //
 
 // Size of world, in cells
 const cols = 25;
@@ -29,69 +41,62 @@ const rows = 25;
 // Size of cells, in pixels
 const cellSize = 20;
 
-// ============================================================ //
 
-// Loaded from config.js
-var elements = {};
+// ==== World and accessors ==== //
 
-// ============================================================ //
+// Column/x, then row/y. Initialized later.
+var world = [];
 
-// Column/x, then row/y
-var world = []
-
-function allPositions() {
-    return Array.from(new Array(cols), (_, x) => x).flatMap(x =>
-        Array.from(new Array(rows), (_, y) => [x, y])
-    );
-}
-
-var visitOrders = generateVisitOrders();
-
-// Generate a handful of randomized orderings of all cell positions.
-function generateVisitOrders() {
-    const all = allPositions();
-    return Array.from(new Array(10), _ => shuffled(all));
-}
-
-// Get a random visit order
-function getVisitOrder() {
-    return visitOrders[Math.floor(visitOrders.length * Math.random())];
-}
-
+// Coordinate transform from DOM event to cell position.
 function canvasToPos(evt) {
     var posX = Math.floor(evt.offsetX / cellSize);
     var posY = Math.floor(evt.offsetY / cellSize);
     return [posX, posY];
 }
 
+
+// ==== Reloadable settings ==== //
+
+// These will actually be overwritten by config.js; they're just declared here to
+// have there be something for a search to find.
+// See config.js for the actual value and documention.
+
+// Definitions of all elements.
+var elements = undefined;
+
+// Element we're currently drawing. UI pending.
+var currentlyDrawing = undefined;
+
+
+// ==== Interactively drawing on the canvas ==== //
+
+// Draw on the indicated cell.
 function doDraw(pos) {
     const [x, y] = pos
-    const dat = world[x][y] = {type: 'rabbit'};
+    const dat = world[x][y] = {type: currentlyDrawing};
     redrawCell(dat, pos);
 }
 
+// Draw on the canvas due to a click.
 function doCanvasClick(evt) {
     if (evt.target != canvas) return;
     doDraw(canvasToPos(evt));
 }
 
+// Draw on the canvas due to a mouse drag.
 function doCanvasMousemove(evt) {
     if (evt.target != canvas) return;
-    if ((evt.buttons & 1) == 0) return;
+    if ((evt.buttons & 1) == 0) return; // only left/main button
     doDraw(canvasToPos(evt));
 }
 
-function redrawCell(dat, pos) {
-    const [x, y] = pos;
-    elements[dat.type].draw(x * cellSize, y * cellSize);
-}
 
-function inWorld(x, y) {
-    return x >= 0 && x < cols && y >= 0 && y < rows;
-}
+// ==== Direction logic ==== //
 
+// Relative directions.
 var DOWN = [0, 1];
 
+// Move in the indicated direction from the position, returning the new position.
 function follow(pos, dir) {
     let [x, y] = pos;
     x += dir[0];
@@ -99,25 +104,45 @@ function follow(pos, dir) {
     return [x, y];
 }
 
+
+// ==== World accessors ==== //
+
+// Ask a cell to redraw itself. Called when this position in the world
+// has been changed and the canvas needs to be updated to reflect it.
+function redrawCell(dat, pos) {
+    const [x, y] = pos;
+    elements[dat.type].draw(x * cellSize, y * cellSize);
+}
+
+// Is this a legal position in the world?
+function inWorld(x, y) {
+    return x >= 0 && x < cols && y >= 0 && y < rows;
+}
+
+// Get a cell {pos, dat} from a position.
 function get(pos) {
     const [x, y] = pos;
     if (inWorld(x, y)) {
-        return {pos: [x, y], dat: world[x][y]};
+        return {pos, dat: world[x][y]};
     } else {
-        return {pos: [x, y], dat: {type: 'edge'}};
+        // Synthetic return value representing off-world coordinates.
+        return {pos, dat: {type: 'edge'}};
     }
 }
 
+// Retrieve a cell in the given direction from this position.
 function look(pos, dir) {
     return get(follow(pos, dir));
 }
 
+// Set a position to this cell data, and redraw it.
 function set(pos, dat) {
     const [x, y] = pos;
     world[x][y] = dat;
     redrawCell(dat, pos);
 }
 
+// Swap these two cells.
 function swap(cell1, cell2) {
     if (cell1.pos[0] == cell2.pos[0] && cell1.pos[1] == cell2.pos[1])
         throw Error("Tried to swap cell into itself");
@@ -127,7 +152,7 @@ function swap(cell1, cell2) {
     set(cell2.pos, data1);
 }
 
-/** Return cell within 1 step, including starting position. */
+// Return any cell within 1 step, including diagonals and current position.
 function anyNeighborhood9(pos) {
     const [x, y] = pos;
     const hr = Math.floor(Math.random() * 3) - 1;
@@ -138,6 +163,36 @@ function anyNeighborhood9(pos) {
     ]);
 }
 
+
+// ==== Cell-visiting ==== //
+
+// Get an array of all [x, y] positions in the world.
+function allPositions() {
+    return range(cols).flatMap(x => range(rows).map(y => [x, y]));
+}
+
+// An array of visit-orders. Each visit-order is a pre-randomized array of all of the
+// positions in the world. Following one of the visit-orders when updating the
+// world ensures we visit every cell, but in a way that doesn't introduce global
+// bias, and without having to randomize the ordering every single time. We
+// probably want to pick a random visit-order for each iteration.
+var visitOrders = precomputeVisitOrders();
+
+// Precompute a handful of randomized orderings of all cell positions.
+function precomputeVisitOrders() {
+    const all = allPositions();
+    return Array.from(new Array(10), _ => shuffled(all));
+}
+
+// Get a random visit order.
+function getVisitOrder() {
+    return visitOrders[Math.floor(visitOrders.length * Math.random())];
+}
+
+
+// ==== Core loop ==== //
+
+// Walk over all positions in the world and update them, in some order.
 function updateWorld() {
     getVisitOrder().forEach(pos => {
         const [x, y] = pos;
@@ -152,8 +207,10 @@ function updateWorld() {
     })
 }
 
+// Handle on the world-updating loop. (setInterval/clearInterval).
 var runner = null;
 
+// Button handler: Toggle between running/paused.
 function doPlayPause() {
     if (runner) {
         // Pause
@@ -167,6 +224,20 @@ function doPlayPause() {
     }
 }
 
+
+// ==== Hot-reloadable config ==== //
+
+// Redraw every cell in the world. Only used when reloading config, not
+// during regular updating.
+function redrawWorld() {
+    for (var x = 0; x < cols; x++) {
+        for (var y = 0; y < rows; y++) {
+            redrawCell(world[x][y], [x, y]);
+        }
+    }
+}
+
+// Reload config.js to get newly written element behavior.
 function doReloadConfig() {
     const s = document.createElement('script');
     s.id = "config-loader";
@@ -178,14 +249,10 @@ function doReloadConfig() {
     document.body.removeChild(s);
 }
 
-function redrawWorld() {
-    for (var x = 0; x < cols; x++) {
-        for (var y = 0; y < rows; y++) {
-            redrawCell(world[x][y], [x, y]);
-        }
-    }
-}
 
+// ==== Initialization ==== //
+
+// Initial page setup
 function initialize() {
     canvas.width = cols * cellSize;
     canvas.height = rows * cellSize;
--- config.js
+++ config.js
@@ -1,22 +1,41 @@
+// This file is a hot-swappable definition of how all the elements
+// work. It is re-run when the user requests it.
+
 "use strict";
 
+// This image needs to be loaded on the page somewhere in order to be able to
+// copy it onto the canvas.
 var rabbitImage = document.getElementById('icon-rabbit');
 if (rabbitImage.width != cellSize || rabbitImage.height != cellSize)
     throw Error("Rabbit image does not match cell size");
 
-// All known types.
+// All known element types.
 //
 // - draw: Given upper-left pixel coordinates of the cell; expected to
 //   completely redraw this cell.
 // - act: Given a cell dict representing the current cell, take whatever
-//   actions this element will take on a time step.
-var elements = {
+//   actions this element will take on a time step. The dictionary
+//   contains an [x, y] pair under 'pos' and a data dict under 'dat',
+//   which should always contain a 'type' string matching the element.
+elements = {
+    // The "edge" element is a synthetic element that is "just beyond" the
+    // edge of the world.
+    'edge': {
+        draw: function(px, py) {
+            throw Error("'edge' element should not be shown.");
+        },
+        act: function(me) {
+            throw Error("'edge' element should not be run.");
+        },
+    },
+
     'air': {
         draw: function(px, py) {
             ctx.fillStyle = 'white';
             ctx.fillRect(px, py, cellSize, cellSize);
         },
     },
+
     'sand': {
         draw: function(px, py) {
             ctx.fillStyle = '#cc0';
@@ -29,6 +48,7 @@ var elements = {
             }
         },
     },
+
     'rabbit': {
         draw: function(px, py) {
             ctx.drawImage(rabbitImage, px, py);
@@ -40,4 +60,6 @@ var elements = {
             }
         },
     },
-}
+};
+
+currentlyDrawing = 'rabbit';
