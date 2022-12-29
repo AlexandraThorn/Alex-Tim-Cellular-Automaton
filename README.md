850aa68c0691f1bb8e12e1fd7d4ac4d0c82fc0e8
Tim McCormack <cortex@brainonfire.net>
2022-12-29 10:00:38 -0500

    Rabbitworld, with play/pause

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 Alex and Tim's cellular automaton.
+
+License information:
+- Rabbit icon copyright Google, licensed under Apache 2
--- ca.js
+++ ca.js
@@ -1,17 +1,21 @@
+var playPause = document.getElementById("play-pause");
+
 var canvas = document.getElementById("world");
 var ctx = canvas.getContext('2d'); // drawing context
 
 // Size of world, in cells
-var cols = 30;
-var rows = 30;
+var cols = 25;
+var rows = 25;
 
 // Size of cells, in pixels
-var cellSize = 8;
+var cellSize = 20;
 
 
 var EMPTY = 0;
 var SAND = 1;
+var RABBIT = 2;
 
+var rabbitImage = document.getElementById('icon-rabbit');
 
 // Column, then row
 var world = []
@@ -23,17 +27,23 @@ function doClick(evt) {
     if (evt.target != canvas) return;
     var cellX = Math.floor(evt.offsetX / cellSize);
     var cellY = Math.floor(evt.offsetY / cellSize);
-    world[cellX][cellY] = SAND;
+    world[cellX][cellY] = RABBIT;
     redrawCell(cellX, cellY);
 }
 
 function redrawCell(x, y) {
-    if (world[x][y] == SAND) {
+    var cur = world[x][y]
+    var ulx = x * cellSize;
+    var uly = y * cellSize;
+    if (cur == SAND) {
         ctx.fillStyle = '#cc0';
+        ctx.fillRect(ulx, uly, cellSize, cellSize);
+    } else if (cur == RABBIT) {
+        ctx.drawImage(rabbitImage, ulx, uly);
     } else {
         ctx.fillStyle = 'white';
+        ctx.fillRect(ulx, uly, cellSize, cellSize);
     }
-    ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
 }
 
 function updateWorld() {
@@ -48,14 +58,47 @@ function updateWorld() {
                     redrawCell(x, y);
                     redrawCell(x, y+1);
                 }
+            } else if (cur == RABBIT) {
+                var hr = Math.floor(Math.random() * 3) - 1;
+                var vr = Math.floor(Math.random() * 3) - 1;
+                var nextX = x + hr;
+                var nextY = y + vr;
+                if (
+                    !(hr == 0 && vr == 0)
+                    && nextX >= 0 && nextX < cols && nextY >= 0 && nextY < rows
+                    && world[nextX][nextY] == EMPTY
+                ) {
+                    world[nextX][nextY] = RABBIT;
+                    world[x][y] = EMPTY;
+                    redrawCell(nextX, nextY);
+                    redrawCell(x, y);
+                }
             }
         }
+    }
+}
+
+var runner = null;
+
+function doPlayPause() {
+    if (runner) {
+        // Pause
+        clearInterval(runner);
+        runner = null;
+        playPause.textContent = "Play";
+    } else {
+        // Play
+        runner = setInterval(updateWorld, 200);
+        playPause.textContent = "Pause";
     }
 }
 
 function initialize() {
     canvas.width = cols * cellSize;
     canvas.height = rows * cellSize;
+
+    world[0][0] = RABBIT;
+    world[5][5] = RABBIT;
 
     for (var x = 0; x < cols; x++) {
         for (var y = 0; y < rows; y++) {
@@ -65,7 +108,7 @@ function initialize() {
 
     canvas.addEventListener('click', doClick);
 
-    setInterval(updateWorld, 100);
+    doPlayPause();
 }
 
 initialize();
